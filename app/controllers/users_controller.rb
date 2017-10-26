1b3ead93caa2f8e8f4d16c3de0460a0a7f681e55
raegertay <raegertay89@gmail.com>
2017-10-26 17:35:01 +0800

    Added avatar upload function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
 
   def update_avatar
-    byebug
+    if current_user.update(user_avatar_params)
+      flash[:notice] = 'Tasklist updated succesfully'
+    else
+      flash[:alert] = "Avatar upload failed"
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def user_avatar_params
+    params.require(:user).permit(:avatar_url)
   end
 
 end
