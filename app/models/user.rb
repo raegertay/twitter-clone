0693483a0bdbdb92b88cb0ab22d5238f11b9c1b3
raegertay <raegertay89@gmail.com>
2017-10-27 23:13:07 +0800

    Added 'followers' association to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :followships
   has_many :followings, through: :followships
+  has_many :inverse_followships, class_name: 'Followship', foreign_key: 'following_id'
+  has_many :followers, through: :inverse_followships, source: :user
 
 end
