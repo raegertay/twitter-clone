2913950cd230f57c614cc2ddd29594a963a61ab7
raegertay <raegertay89@gmail.com>
2017-10-28 10:05:39 +0800

    Added destroy dependence on followship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   has_many :tweets, dependent: :destroy
-  has_many :followships
+  has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
-  has_many :inverse_followships, class_name: 'Followship', foreign_key: 'following_id'
+  has_many :inverse_followships, class_name: 'Followship', foreign_key: 'following_id', dependent: :destroy
   has_many :followers, through: :inverse_followships, source: :user
 
 end
