a3ddfa0c2ade8e32fc5afb44cff9ee62a2bef22f
raegertay <raegertay89@gmail.com>
2017-10-28 12:26:44 +0800

    Only display tweets of user and following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,16 @@ class User < ApplicationRecord
   has_many :inverse_followships, class_name: 'Followship', foreign_key: 'following_id', dependent: :destroy
   has_many :followers, through: :inverse_followships, source: :user
 
+  # Return users not currently following
   def not_following
     ids_to_exclude = self.followings.ids + [self.id]
     User.where.not(id: ids_to_exclude)
+  end
+
+  # Return the tweets belonging to self and self.followings, ordered by desc 'created_at'
+  def home_tweets
+    ids_to_include = self.followings.ids + [self.id]
+    Tweet.where(user_id: ids_to_include).order(created_at: :desc)
   end
 
 end
