9e80bcefe57eef38624014984110baa7d475d41c
raegertay <raegertay89@gmail.com>
2017-10-30 23:34:55 +0800

    Refactored 'follow' and 'unfollow' action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,16 @@ class UsersController < ApplicationController
   end
 
   def follow
-    followee_id = params[:id]
-    @following = current_user.followings.build(followee_id: followee_id)
-    @following.save
-    flash[:notice] = 'Following ' + User.find(followee_id).username + ' now'
+    @followee = User.find(params[:id])
+    @following = current_user.followings.create(followee_id: @followee.id)
+    flash[:notice] = 'Following ' + @followee.username + ' now'
     redirect_back(fallback_location: root_path)
   end
 
   def unfollow
-    unfollow_user = User.find(params[:id])
-    current_user.followees.destroy(unfollow_user)
-    flash[:notice] = 'Unfollowed ' + unfollow_user.username
+    @followee = User.find(params[:id])
+    current_user.followees.destroy(@followee)
+    flash[:notice] = 'Unfollowed ' + @followee.username
     redirect_back(fallback_location: root_path)
   end
 
