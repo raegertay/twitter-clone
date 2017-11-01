229c573f6ef6404a71e537e20b2a06b4cefa3f19
raegertay <raegertay89@gmail.com>
2017-11-01 23:01:21 +0800

    Fixed issue with requiring login message in login page everytime

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
+  authenticated :user do
+    root to: 'pages#home', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
   get 'user/:id/following', to: 'pages#following', as: 'user_following'
   get 'user/:id/follower', to: 'pages#follower', as: 'user_follower'
   get 'user/:id/tweets', to: 'pages#tweets', as: 'user_tweets'
