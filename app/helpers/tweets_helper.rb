c85b63897072147a4aeed7baa45043953824b9f8
raegertay <raegertay89@gmail.com>
2017-11-02 22:17:26 +0800

    Embed tags in tweet body now

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,12 @@
 module TweetsHelper
+
+  def tagify_tweet(tweet)
+    tags = tweet.tags
+    tagified_tweet = tweet.body
+    tags.each do |tag|
+      tagified_tweet.gsub!(tag.body, link_to(tag.body, tweets_by_tag_path(tag)) )
+    end
+    tagified_tweet.html_safe
+  end
+
 end
