a33a08d4d7dc2279180596a56b4b87ac1e622a24
raegertay <raegertay89@gmail.com>
2017-11-05 16:57:43 +0800

    Fixed a bug with Turbolinks where tweets are added multiple times

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,21 +52,6 @@ $(document).on("turbolinks:load", function() {
     $('.filestack-btn').click();
   });
 
-  // Auto AJAX loading of more tweets
-  var justLoaded = false;
-  $(window).scroll(function() {
-    var pageBottomPosition = $('html, body').scrollTop() + $(window).height();
-    var lastTweetPosition = $('.tweets > div:last').offset().top + $('.tweets > div:last').height();
-    if (pageBottomPosition >= lastTweetPosition && $('#next-page-link').length && !justLoaded) {
-      $('#next-page-link')[0].click();
-      console.log("Load new tweets");
-      justLoaded = true;
-      setTimeout(function() {
-        justLoaded = false;
-      }, 500);
-    }
-  });
-
 });
 
 function updateAvatar(data) {
@@ -77,3 +62,35 @@ function updateAvatar(data) {
     data: params
   });
 }
+
+// Auto AJAX loading of more tweets
+var readyToLoad = true;
+var resetLoadStatus = function() {
+  setTimeout(function() {
+    readyToLoad = true;
+    console.log(`Ready to load(reset): ${readyToLoad}`);
+  }, 500);
+};
+
+$(window).scroll(function() {
+  var pageBottomPosition = $('html, body').scrollTop() + $(window).height();
+  var lastTweetPosition = $('.tweets > div:last').offset().top + $('.tweets > div:last').height();
+  console.log(`Ready to load(before if): ${readyToLoad}`);
+  if (readyToLoad && pageBottomPosition >= lastTweetPosition && $('#next-page-link').length) {
+    $('#next-page-link')[0].click();
+    console.log("Loading new tweets");
+    readyToLoad = false;
+    console.log(`Ready to load(after click): ${readyToLoad}`);
+    resetLoadStatus();
+  }
+});
+
+// var timer = function(time) {
+//   console.log(time);
+//   if (time == 0) {
+//     return "Time's up!";
+//   }
+//   setTimeout(function() {
+//     timer(time-1);
+//   }, 1000);
+// };
